b8337f52ebc5e9305ce6b8f5bcf7ee7034f2ca7e
masieroaurelien <masiero.aurelien@outlook.fr>
2020-06-14 02:26:42 +0200

    update 14-06-2020

---
+++ include/menu.php
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<a href="index.php" class="nav"><li>Accueil</li></a>
+		<a href="alternance.php" class="nav"><li>Alternance</li></a>
+		<a href="conseils.php" class="nav"><li>Conseils</li></a>
+		<a href="podcasts.php" class="nav"><li>Podcasts</li></a>
+		<a href="parolesalternants.php" class="nav"><li>Paroles d'alternants</li></a>
+		<a href="contact.php" class="nav"><li>Contact</li></a>
+	</ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+include 'include/if.php';
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	    <title>ALT-R - Accueil</title>
+	    <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- feuille de style -->
+	</head>
+
+	<header>
+	    <?php include 'include/menu.php'; ?>
+	</header>
+
+	<body>
+
+		<div class="container">
+			<img src="medias/index1.jpg" class="index">
+
+			<div class="textimgindex">
+				<title class="indexpres">ALT-R</title>
+				<p class="indexpres">Le site pour muscler ta recherche d'alternance</p>
+			</div>
+		</div>
+
+		<div class="container">
+			<!-- intégrer carousel de Garbiela ici -->
+		</div>
+
+		<div class="container flex">
+
+			<div class="projetaltr">
+
+				<title class="indexpdt">Le Projet ALT-R</title>
+
+				<img src="medias/projetaltr.png" class="indexpdt">
+
+				<p class="indexpdt">Découvre l’histoire du projet ALT-R dédié à la recherche d’alternance.</p>
+
+			</div>
+
+			<div class="discord">
+
+				<title class="indexpdt">Le fruit de la Discord</title>
+
+				<img src="medias/discord.png" class="indexpdt">
+
+				<p class="indexpdt">Tout a commencé lorsque Morgane, une super coach en insertion professionnelle, a créé un channel Discord. Ce dernier permet à ses coachés d’échanger, de s’entraider et de progresser collectivement. C’est comme ça que Gabriela, Aurélien, Thomas et Amélie, 4 alternants d’OpenClassrooms se sont rencontrés virtuellement pour mettre sur pied ALT-R. Toujours accompagnés de leur bonne fée Morgane !</p>
+
+				
+			</div>
+
+			<div class="toux">
+
+				<title class="indexpdt">Une toux qui mêne à tout</title>
+
+				<img src="medias/toux.png" class="indexpdt">
+				
+				<p class="indexpdt">Alors que le COVID-19 condamnait la France à un Confinement à Durée Indéterminée, on a voulu utiliser ce temps à bon escient, en mettant nos expériences et nos compétences complémentaires au service d’un projet utile pour nos pairs. On vient tous d’horizons très différents, mais on est tous en reconversion professionnelle, tous passionnés de tech & digital. C’est pourquoi, on a voulu s’adresser à toi, futur alternant qui est dans la même situation que nous : tu n’es pas seul, tu vas y arriver !</p>
+
+			</div>
+			
+		</div>
+
+		<div class="container">
+			
+		</div>
+
+
+	</body>
+
+</html>
